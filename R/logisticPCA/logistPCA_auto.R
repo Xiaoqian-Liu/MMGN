d56f54b339cb6b590f0feb3d083f4c6f9d4fc1f2
Xiaoqian Liu <xiaoqian.liu1025@gmail.com>
2024-03-30 09:14:29 -0500

    Update logistPCA_auto.R

--- R/logisticPCA/logistPCA_auto.R
+++ R/logisticPCA/logistPCA_auto.R
@@ -24,6 +24,7 @@ logistPCA_auto <- function(Y, ind_omega, sigma=1, rSeq=1:5, rate=0.8, seed=1234)
   f <- function(x) {return(plogis(x, scale = sigma))}
   gradf <- function(x) {return(dlogis(x, scale = sigma))}
   
+  
   # generate training/testing sets
   set.seed(seed)
   omega <- which(ind_omega > 0)
@@ -34,6 +35,14 @@ logistPCA_auto <- function(Y, ind_omega, sigma=1, rSeq=1:5, rate=0.8, seed=1234)
   ind_train <- ind_test <- rep(0, m*n)
   ind_train[training] <- 1
   ind_test[testing] <- 1
+  
+  # for initialization
+  svd_out <- svd(Y)
+  rhat <- 1
+  mll <- -Inf
+  U00 <- svd_out$u[, 1:rhat]%*%sqrt(diag(x=svd_out$d[1:rhat], nrow=rhat))
+  V00 <- svd_out$v[, 1:rhat]%*%sqrt(diag(x=svd_out$d[1:rhat], nrow=rhat))
+  
   
   # get the training matrix for logistPCA
   y_train <- as.vector(Y)
@@ -54,18 +63,31 @@ logistPCA_auto <- function(Y, ind_omega, sigma=1, rSeq=1:5, rate=0.8, seed=1234)
     # set r for this loop
     r <- rSeq[i]
     
-    res <- logisticSVD(D_train, k = r, main_effects = FALSE, partial_decomp = FALSE)
+    U0 <- svd_out$u[, 1:r]%*%sqrt(diag(x=svd_out$d[1:r], nrow=r))
+    V0 <- svd_out$v[, 1:r]%*%sqrt(diag(x=svd_out$d[1:r], nrow=r))
+    
+    
+    res <- logisticSVD(D_train, k = r, start_A = U0, start_B=V0, main_effects = FALSE, partial_decomp = TRUE)
     Mhat <- res$A%*%t(res$B)
     
     # compute the log-likelihood on the testing set
     #loglik[i] <- -loss_1bit(D_obs, Mhat, testing, f)
     loglik[i] <- -obj_1bit(D_obs[ind_test>0], Mhat[ind_test>0], f)
+    
+    # for the overall
+    if (loglik[i]>mll){
+      rhat <- r
+      mll <- loglik[i]
+      U00 <- res$A
+      V00 <- res$B
+    }
+    
   }
   
-  ind <- which.max(loglik)
-  rhat <- rSeq[ind]
+#  ind <- which.max(loglik)
+#  rhat <- rSeq[ind]
   
-  res <- logisticSVD(D_obs, k = rhat, main_effects = FALSE, partial_decomp = FALSE)
+  res <- logisticSVD(D_obs, k = rhat,start_A = U00, start_B=V00, main_effects = FALSE, partial_decomp = TRUE)
 
   
   return(list(U = res$A, V = res$B, rhat = rhat, loglik = loglik))
